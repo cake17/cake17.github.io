7a6ebd6754d781dd86c2a21edac09b746cbaf748
cake17 <cake17@cake-websites.com>
2015-03-12 12:31:42 +0100

    add bower.json and Gemfile

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -3,19 +3,19 @@ $(function () {
         //alert('ee');
     //});
 
-	$(document).keydown(function(event){
-		var key = event.which;
-		switch(key) {
-			case 37:
-				if(typeof $('#jquery-prev').attr('href') !== 'undefined'){
-					window.location.href = $('#jquery-prev').attr('href');
-	 			};
-			break;
-			case 39:
-				if(typeof $('#jquery-next').attr('href') !== 'undefined'){
-					window.location.href = $('#jquery-next').attr('href');
-	 			};
-			break;
-		}
-	});
+    $(document).keydown(function(event){
+        var key = event.which;
+        switch(key) {
+            case 37:
+                if(typeof $('#jquery-prev').attr('href') !== 'undefined'){
+                    window.location.href = $('#jquery-prev').attr('href');
+                 };
+            break;
+            case 39:
+                if(typeof $('#jquery-next').attr('href') !== 'undefined'){
+                    window.location.href = $('#jquery-next').attr('href');
+                 };
+            break;
+        }
+    });
 });
