ca9d68097d53c98f66c7a40a3cdf68a48be2805f
cake17 <cake17@cake-websites.com>
2018-05-08 20:28:34 +0200

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Blog',
     description: 'Dev tips',
+    base: 'http://blog.cake-websites.com',
     themeConfig: {
         nav: false,
         sidebar: [
@@ -9,3 +10,38 @@ module.exports = {
         ]
     }
 }
+
+
+// # Site settings
+// title: Cake17's Blog
+// email: your - email@domain.com
+// description: > # this means to ignore newlines until "baseurl:"
+// Write an awesome description for your new site here.You can edit this
+// line in _config.yml.It will appear in your document head meta(for
+//   Google search results) and in your feed.xml site description.
+//     baseurl: "" # the subpath of your site, e.g. / blog
+// url: "http://blog.cake-websites.com" # the base hostname & protocol for your site
+// author:
+//     name: cake17
+// email: cake17@cake-websites.com
+// github: cake17
+// bio:
+// email_md5: HBfd!8é!çà99098872
+
+// # Build settings
+// markdown: kramdown
+
+// # files to exclude
+// exclude: ["README.md"]
+
+// gems:
+// - jekyll - gist
+
+// defaults:
+// -
+//     scope:
+// path: "_posts" # values will be defined for all files in _posts(empty string for all files)
+//     type: "posts" # apply the defaults only for posts
+//     values:
+//     layout: "post"
+// comments: true
